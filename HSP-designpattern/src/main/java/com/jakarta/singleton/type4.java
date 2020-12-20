7ccfbfd2d3b240b7103c047049718e94bdc730e1
kevinxue025 <kvx025@gmail.com>
2020-12-20 14:51:10 +0800

    第四种单例模式-懒汉式（线程安全，同步方法）

---
+++ HSP-designpattern/src/main/java/com/jakarta/singleton/type4.java
@@ -0,0 +1,28 @@
+package com.jakarta.singleton;
+
+/**
+ * @author Kevin Xue
+ * @create 2020-12-20 14:42
+ * @description:
+ */
+public class type4 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Singleton4{
+    private Singleton4() {
+
+    }
+    private static Singleton4 singleton4;
+
+    // 提供一个静态的公共方法，当使用到的时候再创建instance
+    // 加入了同步处理的synchronized，解决线程安全问题
+    public static synchronized Singleton4 getInstace(){
+        if (singleton4==null) {
+            singleton4 = new Singleton4();
+        }
+        return singleton4;
+    }
+}
