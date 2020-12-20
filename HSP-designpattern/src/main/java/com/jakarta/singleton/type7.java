f9483cffb34988a27294b6b1326d1ba15db51b0d
kevinxue025 <kvx025@gmail.com>
2020-12-20 15:20:19 +0800

    第七种单例模式-懒汉式（静态内部类）

---
+++ HSP-designpattern/src/main/java/com/jakarta/singleton/type7.java
@@ -0,0 +1,28 @@
+package com.jakarta.singleton;
+
+/**
+ * @author Kevin Xue
+ * @create 2020-12-20 17:12
+ * @description: 懒汉式（静态内部类）
+ */
+public class type7 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Singleton7{
+    private Singleton7() {
+
+    }
+    // 静态内部类.类中有静态属性。
+    // 静态内部类只有在使用到时才会进行加载
+    private static class SingletonInstace{
+        private  static final Singleton7 SINGLETON_7 = new Singleton7();
+    }
+
+    // 直接返回 SingletonInstace.SINGLETON_7
+    public static Singleton7 getInstace(){
+        return SingletonInstace.SINGLETON_7;
+    }
+}
